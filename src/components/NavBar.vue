37c9773be478da16b58d1c664886412eadedfa3c
leonaidass <leo22esquivel@gmail.com>
2022-01-28 00:37:45 -0300

    navBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+         <v-app-bar style="padding:0px 90px" color="white" app light elevation="6" elevate-on-scroll>
+        <v-toolbar-title>
+          WebSite with Vuetify
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-list class="d-flex aling-center">
+          <v-list-item link v-for="(item,i) in menusItem" :key="i" :to="menusItem.ruta">
+            <v-list-item-title>
+              {{item.titulo}}
+            </v-list-item-title>
+          </v-list-item>
+          <v-btn text>
+            <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+            <v-btn outlined color="success">
+              Bienvenidos
+            </v-btn>
+        </v-list>
+      </v-app-bar>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                menusItem:[
+          {titulo:'Inicio', ruta:'home'},
+          {titulo:'Sobre Nosotros', ruta:'nosotros'},
+          {titulo:'Portafolio', ruta:'portafolio'},
+          {titulo:'Proyecto', ruta:'proyecto'},
+          {titulo:'Galeria', ruta:'galeria'},
+          {titulo:'Contacto', ruta:'contacto'}
+        ]
+      
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
